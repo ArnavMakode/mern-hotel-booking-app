edb0a5a1763d8f34d0fbc41403b194bf57c818bc
ArnavMakode <arnavmakode11@gmail.com>
2024-02-08 20:13:27 +0530

    First Commit

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="bg-blue-800 py-6">
+      <div className="container mx-auto flex justify-between">
+
+      </div>
+    </div>
+  );
+};
+export default Header;
---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header"
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+        <Header/>
+    </div>
+  )
+}
+export default Layout
