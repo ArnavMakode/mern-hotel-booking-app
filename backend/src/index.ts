4c95562d786da9a264a5ddd6b634d396768a5a99
ArnavMakode <arnavmakode11@gmail.com>
2024-02-15 18:06:47 +0530

    auth login route backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,20 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import userRoutes from "./routes/users"
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to db successfully"))
+  .catch(() => console.log("couldnt connect to db"));
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,10 +1,21 @@
 import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import {check, validationResult} from "express-validator"
+import { error } from "console";
 
 const router = express.Router();
 
-router.post("/register", async (req: Request, res: Response) => {
+router.post("/register", [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters is required").isLength({min: 6}),
+    check("firstName", "firstName is required").isString(),
+    check("lastName", "lastName is required").isString(),
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({message: errors.array()});
+    }
   try {
     let user = await User.findOne({
       email: req.body.email,
@@ -16,7 +27,7 @@ router.post("/register", async (req: Request, res: Response) => {
     await user.save();
 
     const token = jwt.sign(
-      { userID: user.id },
+      { userId: user.id },
       process.env.JWT_SECRET_KEY as string,
       {
         expiresIn: "1d",
