998c803e07902c1b60ef9623b0204fc2144bb9fd
ArnavMakode <arnavmakode11@gmail.com>
2024-03-18 17:36:39 +0530

    added my-bookings page and e2e tests

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -65,4 +65,7 @@ test("should book hotel", async ({ page }) => {
   await expect(page.getByText("Booking Saved!")).toBeVisible({
     timeout: 20000,
   });
+
+  await page.getByRole("link", {name: "My Bookings"}).click();
+  await expect(page.getByText("The Himalayan Hideaway")).toBeVisible();
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Details from "./pages/Details";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -91,6 +92,14 @@ function App() {
               element={
                 <Layout>
                   <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
                 </Layout>
               }
             />
